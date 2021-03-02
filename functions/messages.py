c9de3173b242fd445f311be533dd1b5c1bb8689d
Diana Fuentes <dianasaavedra2315@gmail.com>
2021-03-01 23:03:31 -0600

    try it out

---
+++ functions/messages.py
@@ -0,0 +1,9 @@
+def showMessage(nombres):
+  for nombre in nombres: 
+    msg = f"Hello, {nombre.title()}!. Are you ready to practice?"
+    print(msg)
+
+usuario = ['emiliano', 'juan', 'charlie']
+showMessage(usuario) 
+
+
