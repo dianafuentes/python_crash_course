caef59be8834224a00adb4f06688f079028b502c
Diana Fuentes <dianasaavedra2315@gmail.com>
2021-03-05 19:23:11 -0600

    pg.240-241

--- ch12/ship.py
+++ ch12/ship.py
@@ -4,6 +4,7 @@ class Ship:
   "A class to manage the ship"
   def __init__(self, ai_game):
     self.screen = ai_game.screen 
+    self.settings = ai_game.settings
     self.screen_rect = ai_game.screen.get_rect()
 
     #load the ship image and gets rect. 
@@ -13,13 +14,19 @@ class Ship:
     #start each new ship at the bottom center of the screen 
     self.rect.midbottom = self.screen_rect.midbottom  
 
+    #store a decimal value for the ship's horizontal position
+    self.x = float(self.rect.x)
+
     #movement flag 
     self.moving_right = False 
+    self.moving_left = False
 
-  def update(self)
-  "update the ships position based on the movement flag."
+  def update(self):
+    "update the ships position based on the movement flag."
     if self.moving_right:
       self.rect.x += 1
+    if self.moving_left: 
+      self.rect.x -= 1
   
   def blitme(self):
     "Draw the ship at its current location."
