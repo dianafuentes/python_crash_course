8fed12e6f8947126599f4681f5bd26e6d6defeb0
Diana Fuentes <dianasaavedra2315@gmail.com>
2021-03-05 22:38:57 -0600

    stopped at page 245

--- ch12/ship.py
+++ ch12/ship.py
@@ -23,10 +23,12 @@ class Ship:
 
   def update(self):
     "update the ships position based on the movement flag."
-    if self.moving_right:
-      self.rect.x += 1
-    if self.moving_left: 
-      self.rect.x -= 1
+    if self.moving_right and self.rect.right < self.screen_rect.right:
+      self.x += self.settings.ship_speed
+    if self.moving_left and self.rect.left > 0:
+      self.x -= self.settings.ship_speed 
+    #update rect object from self.x
+    self.rect.x = self.x 
   
   def blitme(self):
     "Draw the ship at its current location."
