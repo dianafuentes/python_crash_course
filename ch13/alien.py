ade8e8cd584fa02ae245353abc8b31d2c7a748ea
Diana Fuentes <dianasaavedra2315@gmail.com>
2021-03-17 22:55:04 -0500

    Copied ch12 into ch13

---
+++ ch13/alien.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+  """A Class to respresent a single alien in the fleet."""
+
+  def __init__(self, ai_game):
+    """Initialize the alien and set its starting position."""
+    super().__init__()
+    self.screen = ai_game.screen 
+
+    # Load the alien image and set its rect attribute 
+    self.image = pygame.image.load('*******')
+    self.rect = self.image.get_rect()
+
+    # Start each new alien near the top left of the screen 
+    self.rect.x = self.rect.width
+    self.rect.y = self.rect.height
+
+    # Store the aliens exact horizontal position
+    self.x = float(self.rect.x)
---
+++ ch13/ship.py
@@ -0,0 +1,36 @@
+import pygame 
+
+# classes have methods (aka functions)
+class Ship: 
+  """A class to manage the ship"""
+  def __init__(self, ai_game):
+    self.screen = ai_game.screen 
+    self.settings = ai_game.settings
+    self.screen_rect = ai_game.screen.get_rect()
+
+    #load the ship image and gets rect. 
+    self.image = pygame.image.load('ship.bmp')
+    self.rect = self.image.get_rect() 
+
+    #start each new ship at the bottom center of the screen 
+    self.rect.midbottom = self.screen_rect.midbottom  
+
+    #store a decimal value for the ship's horizontal position
+    self.x = float(self.rect.x)
+
+    #movement flag 
+    self.moving_right = False 
+    self.moving_left = False
+
+  def update(self):
+    """update the ships position based on the movement flag."""
+    if self.moving_right and self.rect.right < self.screen_rect.right:
+      self.x += self.settings.ship_speed
+    if self.moving_left and self.rect.left > 0:
+      self.x -= self.settings.ship_speed 
+    #update rect object from self.x
+    self.rect.x = self.x 
+  
+  def blitme(self):
+    """Draw the ship at its current location."""
+    self.screen.blit(self.image, self.rect)
