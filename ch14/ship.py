02671ab83f2556f2be2a9e35d9a5067803c66553
Diana Fuentes <dianasaavedra2315@gmail.com>
2021-03-24 23:51:00 -0500

    changed spaceship picture to Emiliano

--- ch14/ship.py
+++ ch14/ship.py
@@ -9,7 +9,7 @@ class Ship:
     self.screen_rect = ai_game.screen.get_rect()
 
     #load the ship image and gets rect. 
-    self.image = pygame.image.load('ship.bmp')
+    self.image = pygame.image.load('IMG_6430.bmp')
     self.rect = self.image.get_rect() 
 
     #start each new ship at the bottom center of the screen 
