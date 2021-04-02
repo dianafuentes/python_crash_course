8e441a159ac57922e14cf73485b9d503b1077ffe
Diana Fuentes <dianasaavedra2315@gmail.com>
2021-04-02 00:44:01 -0500

    ending of chapter 15/ Generating data sets and visualizations

--- project2/ch15/scatterSqaures.py
+++ project2/ch15/scatterSqaures.py
@@ -52,4 +52,7 @@ ax.axis([0, 1100, 0, 1100000])
 #Set size of tick labels
 ax.tick_params(axis='both',which='major', labelsize=14)
 
-plt.show()
+plt.show() 
+
+#this is to save the plot on a seperate file
+#plt.savefig('squares_plot.png', bbox_inches='tight')
