52b2072b18e860f6d6aea68d9d579bb8eb244403
Diana Fuentes <dianasaavedra2315@gmail.com>
2021-04-06 14:45:12 -0500

    mapping data sets with JSON, extracting data, building a worl map

--- project2/ch16/eq_world_map.py
+++ project2/ch16/eq_world_map.py
@@ -1,4 +1,7 @@
-import json 
+import json
+
+from plotly.graph_objs import Scattergeo, Layout 
+from plotly import offline
 
 #Explore the structure of the data 
 filename = 'data/eq_data_1_day_m1.json'
@@ -6,7 +9,6 @@ with open(filename) as f:
   all_eq_data = json.load(f) 
 
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_data))
 
 mags, lons, lats = [], [], []
 for eq_dict in all_eq_dicts:
@@ -18,8 +20,11 @@ for eq_dict in all_eq_dicts:
   lats.append(lat)
 print(mags[:10])
 print(lons[:5])
-print(lats[:5])
+print(lats[:5]) 
 
-readable_file = 'data/readable_eq_data.json'
-with open(readable_file, 'w') as f: 
-  json.dump(all_eq_data, f, indent=4)
+#map the earthquakes 
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title= 'Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename= 'global_earthquakes.html')
