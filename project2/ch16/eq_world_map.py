4c52df78066320d29d0fd7ba2c119532daea91ae
Diana Fuentes <dianasaavedra2315@gmail.com>
2021-04-06 22:28:57 -0500

    end of ch16

--- project2/ch16/eq_world_map.py
+++ project2/ch16/eq_world_map.py
@@ -4,26 +4,45 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 #Explore the structure of the data 
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f: 
   all_eq_data = json.load(f) 
 
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_text = [], [], [], []
 for eq_dict in all_eq_dicts:
   mag = eq_dict['properties']['mag']
   lon= eq_dict['geometry']['coordinates'][0]
   lat = eq_dict['geometry']['coordinates'][1]
+  title = eq_dict['properties']['title']
   mags.append(mag)
   lons.append(lon)
   lats.append(lat)
+  hover_text.append(title)
 print(mags[:10])
 print(lons[:5])
 print(lats[:5]) 
 
 #map the earthquakes 
-data = [Scattergeo(lon=lons, lat=lats)]
+#this is a simpler way to define data
+#data = [Scattergeo(lon=lons, lat=lats)]
+# But this is a better way when wanting to customize the presentation
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': hover_text,
+    'marker': {
+      'size': [5*mag for mag in mags],
+      'color': mags,
+      'colorscale': 'Viridis',
+      'reversescale': True, 
+      'colorbar': {'title': 'Magnitude'},
+    
+    },
+ }]
+
 my_layout = Layout(title= 'Global Earthquakes')
 
 fig = {'data': data, 'layout': my_layout}
